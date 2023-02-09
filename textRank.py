ae6ec95612039c523868cb770d7b06c9c32c3ef9
divyansh-tripathi7 <85444838+divyansh-tripathi7@users.noreply.github.com>
2023-02-09 15:29:55 +0530

    Create textRank.py

---
+++ textRank.py
@@ -0,0 +1,18 @@
+from sumy.parsers.plaintext
+import PlaintextParser
+from sumy.nlp.tokenizers
+import Tokenizer
+from sumy.summarizers.lex_rank
+import LexRankSummarizer
+def sumy_method(text):
+  parser = PlaintextParser.from_string(text, Tokenizer("english"))
+summarizer = LexRankSummarizer()
+#Summarize the document with 2 sentences
+summary = summarizer(parser.document, 2)
+dp = []
+for i in summary:
+  lp = str(i)
+dp.append(lp)
+final_sentence = ' '.join(dp)
+return final_sentence
+
